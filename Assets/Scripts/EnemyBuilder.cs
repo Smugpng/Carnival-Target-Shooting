1acbb44c2869d07b83e4d07dac8a14d702dc42e4
JMUCF <ju398578@ucf.edu>
2024-10-16 15:18:41 -0400

    stuff

---
+++ Assets/Scripts/EnemyBuilder.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyBuilder : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float speed;
+    private float size;
+    private int point;
+
+    enum speedOptions {slow, medium, fast};
+    enum sizeOptions {average, medium, large};
+    enum pointOptions {small, medium, large};
+
+    void Start()
+    {
+        MakeEnemy();
+    }
+
+    void MakeEnemy()
+    {
+        speedVal = Random.Range(0, 2);
+        GameObject newEnemy = Instantiate(enemyPrefab);
+        newEnemy.GetComponent<EnemyPoint>().speed = speed;
+    }
+}
--- Assets/Scripts/EnemyPoint.cs
+++ Assets/Scripts/EnemyPoint.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyPoint : MonoBehaviour
 {
     public delegate void OnDeath(float points);
     public static event OnDeath onDeath;
+
+    public float speed;
+    public float size;
+    public int point;
 
     [Header("Points I give")]
     public float points;
@@ -14,6 +20,7 @@ public class EnemyPoint : MonoBehaviour
     {
         onDeath?.Invoke(points);
     }
+
     private void Start()
     {
         Invoke("OnDestroy", 5);
