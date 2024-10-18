1587110f5750593cf32dd0b8662c4f0e6aee0846
JMUCF <ju398578@ucf.edu>
2024-10-18 15:40:16 -0400

    Changed enemy builder
    Gets random values and assigns them based on the enums

--- Assets/Scripts/EnemyBuilder.cs
+++ Assets/Scripts/EnemyBuilder.cs
@@ -21,8 +21,58 @@ public class EnemyBuilder : MonoBehaviour
 
     void MakeEnemy()
     {
-        //speedVal = Random.Range(0, 2);  
+        GetValues();
         GameObject newEnemy = Instantiate(enemyPrefab);
         newEnemy.GetComponent<EnemyPoint>().speed = speed;
+        newEnemy.GetComponent<EnemyPoint>().size = size;
+        newEnemy.GetComponent<EnemyPoint>().point = point;
+        Debug.Log("Building enemy with speed: " + speed + ", size: " + size + ", point: " + point);
+
+    }
+
+    void GetValues()
+    {
+        int speedNum = Random.Range(0, 3);
+        int sizeNum = Random.Range(0, 3);
+        int pointNum = Random.Range(0, 3);
+
+        switch ((speedOptions)speedNum)
+        {
+            case speedOptions.slow:
+                speed = 2f;
+                break;
+            case speedOptions.medium:
+                speed = 4f;
+                break;
+            case speedOptions.fast:
+                speed = 6f;
+                break;
+        }
+
+        switch ((sizeOptions)sizeNum)
+        {
+            case sizeOptions.average:
+                size = 1f;
+                break;
+            case sizeOptions.medium:
+                size = 1.5f;
+                break;
+            case sizeOptions.large:
+                size = 2f;
+                break;
+        }
+
+        switch ((pointOptions)pointNum)
+        {
+            case pointOptions.small:
+                point = 10;
+                break;
+            case pointOptions.medium:
+                point = 20;
+                break;
+            case pointOptions.large:
+                point = 30;
+                break;
+        }
     }
 }
